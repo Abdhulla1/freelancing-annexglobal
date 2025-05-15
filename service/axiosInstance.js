64e69a8ffa70207f90ace1f1f94dcf2dc46b78fe
Abdhulla1 <abdhulla.code@gmail.com>
2025-05-15 19:27:00 +0530

    update

--- service/axiosInstance.js
+++ service/axiosInstance.js
@@ -1,7 +1,7 @@
 // service/axiosInstance.js
 import axios from "axios";
 
-const BASE_URL="http://139.59.15.8:8003/api/v1";
+const BASE_URL="https://139.59.15.8:8003/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
